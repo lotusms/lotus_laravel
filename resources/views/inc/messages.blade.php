8d952c1b9fe5f3002a39ffc7892e306db18e8e2b
Luis Silva <lotusms@outlook.com>
2020-01-23 15:47:16 -0500

    changes to env

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,7 +1,7 @@
 @if(count($errors) > 0)
   @foreach($errors->all() as $error)
-  <div uk-grid>
-    <div clas="uk-container uk-container-small">
+  <div uk-grid class="justify-content-center uk-position-top" style="top:96px;">
+    <div clas="uk-container uk-container-medium">
       <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{$error}}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -14,8 +14,8 @@
 @endif
 
 @if(session('success'))
-  <div uk-grid>
-    <div clas="uk-container uk-container-small">
+  <div uk-grid class="justify-content-center uk-position-top" style="top:96px;">
+    <div clas="uk-container uk-container-medium">
   <div class="alert alert-success alert-dismissible fade show" role="alert">
     {{session('success')}}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -27,8 +27,8 @@
 @endif
 
 @if(session('error'))
-  <div uk-grid>
-    <div clas="uk-container uk-container-small">
+  <div uk-grid class="justify-content-center uk-position-top" style="top:96px;">
+    <div clas="uk-container uk-container-medium">
       <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{session('error')}}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
