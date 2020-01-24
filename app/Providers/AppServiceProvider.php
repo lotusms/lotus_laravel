8d332b55263919c66785a26b325782ae009c7867
Luis Silva <lotusms@outlook.com>
2020-01-23 22:44:40 -0500

    added padding to navbar menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema:: defaultStringLength(191);
         // Resource::withoutWrapping();
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
