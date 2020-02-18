1f3cb958dac8a075c8f3a25d8d948e6aca487815
Luis Silva <lotusms@outlook.com>
2020-02-18 18:40:24 -0500

    changes to description

--- resources/js/components/home/MainContent.vue
+++ resources/js/components/home/MainContent.vue
@@ -1,24 +1,24 @@
 <template>
-    <div
-        class="card text-center p-5"
-        style="background-color: #0f2027; border-color: #0f2027; color: #cbccce;"
-    >
-        <h1 class="white-text font-weight-medium">{{ heading }}</h1>
-        <h2 class="green-text playball">{{ subheading }}</h2>
-        <p class="white-text paragraph">{{ text }}</p>
-    </div>
+	<div
+		class="card text-center p-5"
+		style="background-color: #0f2027; border-color: #0f2027; color: #cbccce;"
+	>
+		<h1 class="white-text font-weight-medium">{{ heading }}</h1>
+		<h2 class="green-text playball">{{ subheading }}</h2>
+		<p
+			class="white-text paragraph"
+		>We do not stop working until your web design, and web design requirements have met your satisfaction. Once we receive all the details concerning your web design, we will do as many revisions as needed to achieve the desired web design. Our new web design process is designed to gather all the necessary information that we will need to create the most accurate design for your business, brand, and product type. The criteria carefully scrutinizes the smallest details about your business, as well as search engine trends that allow us to create a website that is not only beautiful, but very efficient, SEO-friendly, and user-friendly.</p>
+	</div>
 </template>
 
 <script>
 export default {
-    name: "MainContent",
-    data() {
-        return {
-            heading: "Web Design & User Experience Experts",
-            subheading: "Ask About Our 100% Web Design Satisfaction Guarantee!",
-            text:
-                "We do not stop working until your web design, and web design requirements have met your satisfaction. Once we receive all the details concerning your web design, we will do as many revisions as needed to achieve the desired web design. Our new web design process is designed to gather all the necessary information that we will need to create the most accurate design for your business, brand, and product type. The criteria carefully scrutinize the smallest details about your business, as well as search engine trends that allow us to create a website that is not only beautiful, but very efficient, SEO-friendly, and user-friendly."
-        };
-    }
+	name: "MainContent",
+	data() {
+		return {
+			heading: "Web Design & User Experience Experts",
+			subheading: "Ask About Our 100% Web Design Satisfaction Guarantee!"
+		};
+	}
 };
 </script>
