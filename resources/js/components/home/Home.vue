66e18a584ffe91ef9d0d42fec77191e867493d53
Luis Silva <lotusms@outlook.com>
2020-03-22 11:54:05 -0400

    covid19 update

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -4,7 +4,7 @@
 			<div
 				class="uk-card uk-card-default uk-card-body"
 				style="
-                    background: rgba(23, 45, 52, 0.58) url(/img/covid19.jpg) no-repeat center center;
+                    background: linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.77)), url(/img/covid19.jpg) no-repeat center center;
                     color: rgb(255, 255, 255);
                     background-size: cover;
                     height: 100vh;
@@ -12,19 +12,21 @@
                     border-bottom: 4px solid #7cb342;
                     "
 			>
-				<h3
-					class="uk-card-title"
-					style="text-align: center; font-size: 40px;"
-				>Coronavirus (COVID-19) Update</h3>
-				<p>
-					The recent pandemic of COVID-19 has created many challenges for small business owners. Some of which have been forced to pause operations to minimize the risk of further spreading the virus.
-					<br />
-					<br />We want our customers to know that at LOTUS Marketing Solutions, we are doing everything in our power to enforce the "Stay Home/Save Lives" movement and we are #QUARANTEAM. We have always been a remote online based company and this has not created any last minute arrangements for us. We will continue to service our clients as usual and in a timely manner.
-					<br />
-					<br />Furthermore, if your organization has been affected by COVID-19. We want you to know that we are here to help in everything we can. We can explore opprotunities to allow you to run your business from home, generate sales, collect customer's information or files, or absorb some protion of your normal business so that you can remain at least partially profitable.
-					<br />
-					<br />Please fell free to reach out to us to discuss further. If your finances have been affected, we can extend a grace period to allow any new work on your site to be paid in smaller placements at a discounted rate, or postpone the due date of any new work on your sites for when you are more stable financially.
-				</p>
+				<div style="background: #000000">
+					<h3
+						class="uk-card-title"
+						style="text-align: center; font-size: 40px;"
+					>Coronavirus (COVID-19) Update</h3>
+					<p>
+						The recent pandemic of COVID-19 has created many challenges for small business owners. Some of which have been forced to pause operations to minimize the risk of further spreading the virus.
+						<br />
+						<br />We want our customers to know that at LOTUS Marketing Solutions, we are doing everything in our power to enforce the "Stay Home/Save Lives" movement and we are #QUARANTEAM. We have always been a remote online based company and this has not created any last minute arrangements for us. We will continue to service our clients as usual and in a timely manner.
+						<br />
+						<br />Furthermore, if your organization has been affected by COVID-19. We want you to know that we are here to help in everything we can. We can explore opprotunities to allow you to run your business from home, generate sales, collect customer's information or files, or absorb some protion of your normal business so that you can remain at least partially profitable.
+						<br />
+						<br />Please fell free to reach out to us to discuss further. If your finances have been affected, we can extend a grace period to allow any new work on your site to be paid in smaller placements at a discounted rate, or postpone the due date of any new work on your sites for when you are more stable financially.
+					</p>
+				</div>
 			</div>
 		</div>
 		<main-slider />
